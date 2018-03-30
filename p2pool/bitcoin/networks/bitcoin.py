1c9abe7142e87df2338ed64f8ae38c3a1c15ae48
dyavolick <dyavolick@gmail.com>
2018-03-30 23:30:33 +0300

    Revert "test"
    This reverts commit 11cf9a8b193af1e820f405611a954cbc40e59d32.

--- p2pool/bitcoin/networks/bitcoin.py
+++ p2pool/bitcoin/networks/bitcoin.py
@@ -8,21 +8,21 @@ from p2pool.util import pack
 
 
 P2P_PREFIX = 'f9beb4d9'.decode('hex')
-P2P_PORT = 35333
+P2P_PORT = 8333
 ADDRESS_VERSION = 0
-RPC_PORT = 8311
+RPC_PORT = 8332
 RPC_CHECK = defer.inlineCallbacks(lambda bitcoind: defer.returnValue(
-            (yield helper.check_genesis_block(bitcoind, '000000009721ce4bb0480e087862bad683c7273ee78dfab4dba1496a152780b9')) and
+            (yield helper.check_genesis_block(bitcoind, '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f')) and
             (yield bitcoind.rpc_getblockchaininfo())['chain'] != 'test'
         ))
 SUBSIDY_FUNC = lambda height: 50*100000000 >> (height + 1)//210000
 POW_FUNC = data.hash256
 BLOCK_PERIOD = 600 # s
-SYMBOL = 'BOL'
-CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoinold') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoinold/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoinold'), 'bitcoinold.conf')
-BLOCK_EXPLORER_URL_PREFIX = '#'
-ADDRESS_EXPLORER_URL_PREFIX = '#'
-TX_EXPLORER_URL_PREFIX = '#'
+SYMBOL = 'BTC'
+CONF_FILE_FUNC = lambda: os.path.join(os.path.join(os.environ['APPDATA'], 'Bitcoin') if platform.system() == 'Windows' else os.path.expanduser('~/Library/Application Support/Bitcoin/') if platform.system() == 'Darwin' else os.path.expanduser('~/.bitcoin'), 'bitcoin.conf')
+BLOCK_EXPLORER_URL_PREFIX = 'https://blockchain.info/block/'
+ADDRESS_EXPLORER_URL_PREFIX = 'https://blockchain.info/address/'
+TX_EXPLORER_URL_PREFIX = 'https://blockchain.info/tx/'
 SANE_TARGET_RANGE = (2**256//2**32//1000000 - 1, 2**256//2**32 - 1)
 DUMB_SCRYPT_DIFF = 1
 DUST_THRESHOLD = 0.001e8
